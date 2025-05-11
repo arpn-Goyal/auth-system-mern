9335a13a2315818f9b232d822e32a360b37a212a
arpn-Goyal <arpangoyal95@gmail.com>
2025-05-11 17:08:17 +0530

    update Dashboard

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -19,7 +19,7 @@ const Dashboard = () => {
   
   const handleLogout = ()=>{
     localStorage.removeItem("token");
-    navigate("/login");
+    navigate("/");
   }
   return (
     <div className="min-h-screen flex justify-center items-center bg-gray-100">
